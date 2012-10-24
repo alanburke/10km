b84f35953438ec4b639cbf1ccb113b551e5109d3
Alan Burke <alan@cycletraffic.com>
2012-10-24 22:28:45 +0100

    Make colorbox responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
   //Examples of how to assign the ColorBox event to elements
-  $("ul.image-gallery li a").colorbox({rel:'group1'});
+  $("ul.image-gallery li a").colorbox({
+    rel:'group1',
+    maxWidth: "98%",
+    maxHeight: "98%",
+  });
 });
