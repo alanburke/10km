b26e943f6fd36b47e123cc2494a0ac4891679337
Alan Burke <alan@cycletraffic.com>
2012-11-05 21:00:13 +0000

    Use tempo to render the news from main site

--- js/main.js
+++ js/main.js
@@ -5,4 +5,14 @@ $(document).ready(function(){
     maxWidth: "98%",
     maxHeight: "98%",
   });
+
+  var news = Tempo.prepare('news').notify(function(event) {
+    if (event.type === TempoEvent.Types.RENDER_STARTING || event.type === TempoEvent.Types.RENDER_COMPLETE) {
+      $('ol').toggleClass('loading');
+    }
+  });
+  news.starting();
+   $.getJSON("http://www.athenryac.com/json/news/4?callback=?", function(data) {
+    news.render(data);
+  });
 });
