f73cfd2235e4a19f3f05bed7939eee2d95921eb0
Alan Burke <alan@cycletraffic.com>
2012-11-05 21:23:11 +0000

    Test for news div before running tempo

--- js/main.js
+++ js/main.js
@@ -6,13 +6,15 @@ $(document).ready(function(){
     maxHeight: "98%",
   });
 
-  var news = Tempo.prepare('news').notify(function(event) {
-    if (event.type === TempoEvent.Types.RENDER_STARTING || event.type === TempoEvent.Types.RENDER_COMPLETE) {
-      $('ol').toggleClass('loading');
-    }
-  });
-  news.starting();
-   $.getJSON("http://www.athenryac.com/json/news/4?callback=?", function(data) {
-    news.render(data);
-  });
+  if ($("#news").length != 0) {
+    var news = Tempo.prepare('news').notify(function(event) {
+      if (event.type === TempoEvent.Types.RENDER_STARTING || event.type === TempoEvent.Types.RENDER_COMPLETE) {
+        $('ol').toggleClass('loading');
+      }
+    });
+    news.starting();
+     $.getJSON("http://www.athenryac.com/json/news/4?callback=?", function(data) {
+      news.render(data);
+    });
+  }
 });
