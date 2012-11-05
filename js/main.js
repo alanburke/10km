9b85d0efe625e6e1cf0e1815fef07feb9b759a1b
Alan Burke <alan@cycletraffic.com>
2012-11-05 20:50:53 +0000

    Correct comment spacing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  //Examples of how to assign the ColorBox event to elements
+  // Examples of how to assign the ColorBox event to elements
   $("ul.image-gallery li a").colorbox({
     rel:'group1',
     maxWidth: "98%",
